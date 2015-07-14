ead274ece0199bf1ea2bef611823eab1df452611
nna <nico.nardone@gmail.com>
2015-07-15 00:18:49 +0200

    same header behaviour iOS & android

--- www/js/app.js
+++ www/js/app.js
@@ -20,6 +20,14 @@ angular.module('risebox', ['ionic', 'risebox.controllers'])
   });
 })
 
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.backButton.previousTitleText(false);
+  $ionicConfigProvider.backButton.text('').icon('ion-chevron-left');
+  $ionicConfigProvider.navBar.alignTitle("center"); //Places them at the bottom for all OS
+  $ionicConfigProvider.tabs.position("bottom"); //Places them at the bottom for all OS
+  $ionicConfigProvider.tabs.style("standard"); //Makes them all look the same across all OS
+})
+
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
