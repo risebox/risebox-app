0ff4d88ba0bd7ed0e48d9bf013b29466597e81fa
nna <nico.nardone@gmail.com>
2015-08-06 00:26:43 +0200

    add cordova-plugin-file-transfert

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('risebox', ['ionic', 'risebox.controllers', 'risebox.services'])
+angular.module('risebox', ['ionic', 'ngCordova', 'risebox.controllers', 'risebox.services'])
+
+.constant('ApiEndpoint', {
+  url: 'https://rbnna-api.herokuapp.com'
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
