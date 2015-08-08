35051a9b1e282bf3be1cbf989f9c487ad3d1d1b1
nna <nico.nardone@gmail.com>
2015-08-08 22:07:53 +0200

    refactor config & constants

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+angular.module('risebox', ['ionic', 'ngCordova', 'risebox.constants', 'risebox.config', 'risebox.controllers', 'risebox.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
+
+;
