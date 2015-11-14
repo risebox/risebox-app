b14415097f0deea8be7636ca72ac5cb81d8ab9ec
nna <nico.nardone@gmail.com>
2015-11-14 19:12:47 +0100

    app can be updated manually updated before registering device

--- www/js/controllers/register_device.controller.js
+++ www/js/controllers/register_device.controller.js
@@ -1,6 +1,6 @@
 angular.module('risebox.controllers')
 
-.controller('RegisterDeviceCtrl', function($scope, $state, $ionicPopup, RiseboxApi, RiseboxObj, Ionic) {
+.controller('RegisterDeviceCtrl', function($scope, $state, $ionicPopup, RiseboxApi, RiseboxObj, Ionic, Versioning) {
 
   $scope.registerDevice = function(user) {
     RiseboxApi.registerDevice(user.email, user.secret, {"origin": ionic.Platform.device()})
@@ -31,5 +31,44 @@ angular.module('risebox.controllers')
                   });
                 })
   };
+
+  $scope.checkForUpdates = function() {
+    Versioning.checkForUpdates(function(hasUpdate){
+      console.log('hasUpdate '+hasUpdate);
+      $scope.hasUpdate = hasUpdate
+    });
+  };
+
+  var confirmReload = function() {
+    $ionicPopup.confirm({
+      title: 'Mise à jour prête',
+      template: "Votre app est à prête. L'application va maintenant redémarrer."
+    }).then(function(res) {
+      if(res) {
+        Versioning.finishUpdate();
+      } else {
+        //RAS
+      }
+    });
+  };
+
+  $scope.updateApp = function() {
+    $scope.progressPct = 0;
+    $scope.showProgress = true;
+    Versioning.prepareUpdate(function(res){
+      $scope.showProgress = false;
+      confirmReload();
+    }, function(err){
+      $ionicPopup.alert({
+        title: "Erreur :(",
+        template: "Une erreur est survenue lors de la mise à jour : essayez plus tard."
+      });
+      $scope.showProgress = false;
+    }, function(prog){
+      $scope.progressPct = prog;
+    });
+  }
+
+  $scope.checkForUpdates();
 
 })
