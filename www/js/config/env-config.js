d13ce7f012b674485a28dd10e52a7065fe66cd4a
nna <nico.nardone@gmail.com>
2015-11-27 01:05:07 +0100

    prod conf

--- www/js/config/env-config.js
+++ www/js/config/env-config.js
@@ -1,9 +1,9 @@
 angular.module('risebox.config')
 
 .constant('EnvConfig', {
-  RISEBOX_API_ENDPOINT: { url: 'https://rbdev-api.herokuapp.com' },
-  IONIC_DEPLOY_CHANNEL: 'dev',
-  STRIP_WAITING_TIME: '3'
+  RISEBOX_API_ENDPOINT: { url: 'https://risebox-api.herokuapp.com' },
+  IONIC_DEPLOY_CHANNEL: 'production',
+  STRIP_WAITING_TIME: '60'
 })
 
 ;
